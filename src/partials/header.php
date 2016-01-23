fc12662b112547af47811bc0e463740aa8716d61
Marco Fugaro <marco.fugaro@gmail.com>
2016-01-23 22:51:19 +0100

    updated php files

---
+++ src/partials/header.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1, minimum-scale=1">
+
+    <title></title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <meta name="copyright" content="&copy;">
+    
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="shortcut icon" href="images/favicon.png" type="image/png">
+
+
+    <!-- IE -->
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/css3polyfill/1.0.0/css3polyfill.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <!--[if lt IE 8]>
+        <div class="browserupgrade">
+            <p>You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+            <p>Stai usando un browser <strong>obsoleto</strong>. Per favore <a href="http://browsehappy.com/">aggiorna il tuo browser</a> per una migliore navigazione.</p>
+        </div>
+    <![endif]-->
