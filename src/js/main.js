996d2a3e8f20e3c9f1a45d5195c48598450c072e
Marco Fugaro <marco.fugaro@gmail.com>
2016-01-31 10:56:28 +0100

    minor fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,4 @@
 $(document).ready(function() {
-/*----------------------------------------------
-            IF IOS LOAD FASTCLICK.JS
------------------------------------------------*/
-    
-    var iOS = ( navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false );
-    if(iOS) $.getScript('js/fastclick.js');
+
 
 });
