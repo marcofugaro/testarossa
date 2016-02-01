2c6ede638e3b11f38865d19fccfca31b373b96e7
Marco Fugaro <marco.fugaro@gmail.com>
2016-02-02 00:02:40 +0100

    removed drop console

--- tasks/browserify.js
+++ tasks/browserify.js
@@ -47,7 +47,7 @@ module.exports = function(config) {
                 .pipe(source(config.browserify.bundleName))
                 .pipe(buffer())
                 .pipe(gulpif(config.browserify.sourcemaps, sourcemaps.init({ loadMaps: true })))
-                .pipe(gulpif(global.isProduction, uglify({ compress: { drop_console: true /*why??*/ } })))
+                .pipe(gulpif(global.isProduction, uglify()))
                 .pipe(gulpif(config.browserify.sourcemaps, sourcemaps.write('./')))
                 .pipe(gulp.dest(config.scripts.dest))
                 .pipe(gulpif(config.browserSync.autoreload, browserSync.stream()));
