f415bcfa9e672226b3f3c9601a5d35aeef7b6645
Marco Fugaro <marco.fugaro@gmail.com>
2016-02-06 00:14:08 +0100

    added markup to watch

--- tasks/watch.js
+++ tasks/watch.js
@@ -6,9 +6,9 @@ module.exports = function(config) {
 
     gulp.task('watch', ['browser-sync'], function() {
 
-        // Scripts are automatically watched and rebundled by Watchify inside Browserify task
-        gulp.watch(config.scripts.src, ['lint']);
+        gulp.watch(config.markup.src,  ['markup']);
         gulp.watch(config.styles.src,  ['styles']);
+        gulp.watch(config.scripts.src, ['lint']);
         gulp.watch(config.images.src,  ['images']);
         gulp.watch(config.fonts.src,   ['fonts']);
         
