2fb95dafdef7270422d42b5939fd734f8fa00f7d
Marco <marco.fugaro@gmail.com>
2016-03-30 10:59:00 +0200

    Updated meta viewport

--- src/partials/header.php
+++ src/partials/header.php
@@ -2,7 +2,7 @@
 <html lang="">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1, minimum-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title></title>
     <meta name="description" content="">
