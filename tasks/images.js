95077c5e2db78a51bdf72d378880708fa8a11754
Marco Fugaro <marco.fugaro@gmail.com>
2016-08-28 01:35:17 +0200

    added webp support

--- tasks/images.js
+++ tasks/images.js
@@ -1,21 +1,22 @@
 import gulp from 'gulp';
 import changed from 'gulp-changed';
-import gulpif from 'gulp-if';
 import imagemin from 'gulp-imagemin';
 import pngquant from 'imagemin-pngquant';
+import webp from 'gulp-webp';
+import gulpif from 'gulp-if';
 import browserSync from 'browser-sync';
 
 import config from './../gulpfile.babel';
 
 
 gulp.task('images', () => {
-  return gulp.src(config.images.src, { base: config.sourceDir }) // TODO fix this base
+  return gulp.src(config.images.src)
     .pipe(changed(config.images.dest))
-    .pipe(gulpif(global.isProduction, imagemin({
-      use: [pngquant()],  // TODO test this or the default png optimizer, and ADD svg optimizer
-      progressive: true,
-      interlaced: true,
-    })))
+    .pipe(imagemin({
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest(config.images.dest))
+    .pipe(webp())
     .pipe(gulp.dest(config.images.dest))
     .pipe(gulpif(config.autoreload, browserSync.stream()));
 });
