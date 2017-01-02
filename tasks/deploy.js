c36a553e8ee988d058f919233d3e3667ca18d6a4
Marco Fugaro <marco.fugaro@gmail.com>
2017-01-02 15:41:30 +0100

    updated deploy task with gulp-gh-pages

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -1,20 +1,10 @@
 import gulp from 'gulp';
-import sftp from 'gulp-sftp';
-import dotenv from 'dotenv';
+import ghPages from 'gulp-gh-pages';
 
 import config from './../gulpfile.babel';
 
 
 gulp.task('deploy', ['build'], () => {
-  // let's read the .env file
-  dotenv.config();
-
-  return gulp.src(config.buildDir)
-    .pipe(sftp({
-      host: process.env.SFTP_HOST,
-      port: process.env.SFTP_PORT,
-      user: process.env.SFTP_USER,
-      pass: process.env.SFTP_PASS,
-      remotePath: process.env.SFTP_REMOTEPATH,
-    }));
+  return gulp.src(`${config.buildDir}/**/*`)
+    .pipe(ghPages());
 });
